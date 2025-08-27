b00ce819a8d1ce6d0da2d49e21046a66d409f7cf
Soumyamanna123 <soumyamanna729@gmail.com>
2025-08-27 12:35:58 +0530

    add

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Browse = () => {
   return (
-    <div>Browse</div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind + CRA</h1>
+    </div>
   )
 }
 
