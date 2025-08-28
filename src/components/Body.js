4a6aba978d4a0e4f975895f8d47509691b7233ee
Soumyamanna123 <soumyamanna729@gmail.com>
2025-08-28 11:17:40 +0530

    add

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import Login from './Login'
-import Browse from './Browse'
+import React from "react";
+import Login from "./Login";
+import Browse from "./Browse";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: " /browse",
+      element: <Browse/>,
+    },
+  ]);
   return (
-    <div > 
-        <Login/>
-        <Browse/>
+    <div>
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { LOGO } from "../utils/constant";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
